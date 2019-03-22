9ec62a48617a72c7ddc22a47b5256953a58f5054
Raphael Strotz <Xzelsius@users.noreply.github.com>
2019-03-22 23:32:41 +0100

    Added service implementation

--- src/RS.KeepAlive/KeepAliveService.cs
+++ src/RS.KeepAlive/KeepAliveService.cs
@@ -1,13 +1,79 @@
 // Copyright (c) Raphael Strotz. All rights reserved.
 
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace RS.KeepAlive
 {
     internal class KeepAliveService : BackgroundService
     {
-        protected override Task ExecuteAsync(CancellationToken stoppingToken) => Task.CompletedTask;
+        private const int Intermission = 1 * 1000;
+
+        private readonly ILogger<KeepAliveService> _logger;
+        private readonly KeepAliveOptions _options;
+
+        public KeepAliveService(ILogger<KeepAliveService> logger, IOptions<KeepAliveOptions> optionsAccessor)
+        {
+            _logger = logger;
+            _options = optionsAccessor.Value;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            if (_options.Targets == null || _options.Targets.Length == 0) return;
+
+            // Initial delay (give the application some startup time)
+            await Task.Delay(Intermission, stoppingToken);
+
+            DateTime? lastExecution = null;
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                if (DateTime.Now.AddMinutes(_options.Interval * -1) < lastExecution)
+                {
+                    await Task.Delay(Intermission, stoppingToken);
+                    continue;
+                }
+
+                var workers = _options.Targets
+                    .Select(
+                        async target =>
+                        {
+                            await Work(target, stoppingToken);
+                        });
+
+                await Task.WhenAny(
+                    Task.WhenAll(workers.ToArray()),
+                    Task.Delay(Timeout.Infinite, stoppingToken));
+
+                lastExecution = DateTime.Now;
+            }
+        }
+
+        private async Task Work(string url, CancellationToken stoppingToken)
+        {
+            using (_logger.BeginScope($"Worker for '{url}'"))
+            {
+                try
+                {
+                    var watch = Stopwatch.StartNew();
+                    using (var client = new HttpClient())
+                    {
+                        var response = await client.GetAsync(url, stoppingToken);
+                        _logger.LogInformation("Status: {0} ({1}) Duration: {2}", response.StatusCode, (int) response.StatusCode, watch.Elapsed);
+                    }
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError(e, "Error occurred");
+                }
+            }
+        }
     }
 }
